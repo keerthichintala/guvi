59942b9be482a792720af37409c050a642ae133e
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-07-29 12:15:49 +0530

    Create primenum.java

---
+++ keerthi/primenum.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		n=n+1;
+		while(n<m)
+		{
+			boolean flag=false;
+			for(int i=2;i<=(n/2);i++)
+			{
+				if(n%i==0)
+				{
+					flag=true;
+					break;
+				}
+			}
+			if(!flag)
+			System.out.print(n);
+			++n;
+		}
+	}
+}

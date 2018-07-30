23157661c71bd4381a36042f290fb2b330997b79
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-07-30 23:20:45 +0530

    Create Armstronginter.java

---
+++ keerthi/Armstronginter.java
@@ -0,0 +1,44 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+                      int low=s.nextInt();
+                      int high=s.nextInt();
+        for(int number = low + 1; number < high; ++number) {
+
+            if (checkArmstrong(number))
+                System.out.print(number + " ");
+        }
+    }
+
+    public static boolean checkArmstrong(int num) {
+        int digits = 0;
+        int result = 0;
+        int originalNumber = num;
+
+       
+        originalNumber = num;
+
+       
+        while (originalNumber != 0) {
+            int rem = originalNumber % 10;
+            result += rem*rem*rem;
+            originalNumber /= 10;
+        }
+
+        if (result == num)
+            return true;
+
+        return false;
+
+	}
+}

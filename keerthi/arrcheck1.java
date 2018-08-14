bb4fefd26b57243b14bce64132860d9df0516d85
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-08-14 23:11:46 +0530

    Create arrcheck1.java

---
+++ keerthi/arrcheck1.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		int[] a=new int[10];
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==m)
+			count++;
+		}
+		System.out.print(count);
+	}
+}

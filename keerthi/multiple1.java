ff6dedc14d2dd8675a391d1f9b435b35e3ec8da6
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-08-14 19:40:56 +0530

    Create multiple1.java

---
+++ keerthi/multiple1.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int k=s.nextInt();
+		int m=s.nextInt();
+		int n=0;
+		n=k*m;
+		if(n%2==0)
+		System.out.print("even");
+		else
+		System.out.print("odd");
+	}
+}

629b88b6ea2e0c248604cd22d0b5da0de55dbfbe
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-08-15 22:40:23 +0530

    Create addeven2.java

---
+++ keerthi/addeven2.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		int add=0;
+		add=n+m;
+		if(add%2==0)
+		System.out.print("even");
+		else
+		System.out.print("odd");
+	}
+}

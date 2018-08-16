e387c1c32a52b3b12e72e1587a7e89195d1a7ff9
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-08-16 11:28:35 +0530

    Create diffodd1.java

---
+++ keerthi/diffodd1.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		int diff=0;
+		diff=n-m;
+		if(diff%2==0)
+		System.out.print("even");
+		else
+		System.out.print("odd");
+	}
+}

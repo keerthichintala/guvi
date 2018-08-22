fd1df7f148ed6a160710ee0913dbc346bd9a27aa
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-08-22 15:50:45 +0530

    Create opponent.java

---
+++ keerthi2/opponent.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		int k=0;
+		if(m>n)
+		{
+			k=m-n;
+			System.out.print(k);
+		}
+	}
+}

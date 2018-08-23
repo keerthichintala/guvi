0f37fbd94e9d06406bed451b5663532df5a0d126
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-08-23 11:56:50 +0530

    Create decimal3.java

---
+++ keerthi2/decimal3.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		double dl=s.nextDouble();
+		double lb=s.nextDouble();
+		double k=dl=lb;
+		System.out.printf("%.5f",k);
+	}
+}

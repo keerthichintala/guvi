f4eedbbbeddff1b51a34c8c73fb6ffa7bcde7b50
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-09-24 19:16:04 +0530

    Create findnum.java

---
+++ keerthi2/findnum.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int k=s.nextInt();
+		int i=0;
+		int[] arr=new int[10];
+		for(i=0;i<n;i++)
+		{
+			arr[i]=s.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			if(i==k-1)
+			{
+				System.out.print(arr[i]);
+				break;
+			}
+		}
+	}
+}

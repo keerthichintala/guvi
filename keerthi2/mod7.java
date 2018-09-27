731be2f73bd02502268706826ef8da11798952fb
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-09-27 11:32:29 +0530

    Create mod7.java

---
+++ keerthi2/mod7.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		int l=s.nextInt();
+		int k=n*m;
+		int i=k%l;
+		System.out.print(i);
+	}
+}

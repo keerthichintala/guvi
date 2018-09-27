b41bfcc1973b770583d2cc3faf54fd191d8c7405
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-09-27 11:07:49 +0530

    Create reversei.java

---
+++ keerthi2/reversei.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int temp=0,k=0;
+		while(n!=0)
+		{
+		k=n%10;
+		temp=temp*10+k;
+		n=n/10;
+		}
+System.out.print(temp);
+	}
+}

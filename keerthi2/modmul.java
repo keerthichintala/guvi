0951332dceec699709239ff8fd7ec6e84fb31349
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-10-09 22:19:46 +0530

    Create modmul.java

---
+++ keerthi2/modmul.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a,b,c;
+		a=s.nextInt();
+		b=s.nextInt();
+		c=s.nextInt();
+		int res=0;
+		res=(a*b)/c;
+		System.out.print(res);
+	}
+}
